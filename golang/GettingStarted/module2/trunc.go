55ca6ebe7b20747aaf76af6a30a21b21e1de8583
Castro-711 <eric.cassells@flutter.com>
2026-03-25 08:22:59 +0000

    First (well really second) but now finding the first attempt was right using Scan for user input, saving this state before updating

---
+++ golang/GettingStarted/module2/trunc.go
@@ -0,0 +1,5 @@
+package module2
+
+func Trunc(floaty float64) int {
+	return int(floaty)
+}
---
+++ golang/GettingStarted/module2/trunc_test.go
@@ -0,0 +1,18 @@
+package module2
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFloatTruncation1(t *testing.T) {
+	var result interface{} = Trunc(1.23456789)
+	i := result.(int)
+	fmt.Printf("%T\n", i)
+}
+
+func TestFloatTruncation2(t *testing.T) {
+	var result interface{} = Trunc(9876543.21)
+	i := result.(int) // will fail if not an int
+	fmt.Printf("%T\n", i)
+}
